bbb7be93a9057adeac9eacf6ed49b5d39953c9f0
Owen Kitchener <okitchener@my.wctc.edu>
2026-02-02 08:19:47 -0600

    [2.06] Create "click" event listener to display toast

--- discount.js
+++ discount.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
+     document.getElementById('discount-row').addEventListener('click', function(e){
+    e.preventDefault();
+
     bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).show();
+         });
 });
