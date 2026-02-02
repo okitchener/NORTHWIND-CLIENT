43747149dcbcb40513480af3eba231dccddc00ac
Owen Kitchener <okitchener@my.wctc.edu>
2026-02-02 09:26:10 -0600

    Finished Mod2 Assignment

--- discount.js
+++ discount.js
@@ -3,6 +3,19 @@ document.addEventListener("DOMContentLoaded", function() {
      if (e.target.classList.contains('discount')) {
       e.preventDefault();
       document.getElementById('product').innerHTML = e.target.dataset['product'];
+        document.getElementById('title').innerHTML = e.target.dataset['title'];
+        document.getElementById('discount').innerHTML = e.target.dataset['discount'];
       bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).show();
     }  });
-});
+   
+}); 
+function closeToast() {
+    var toast = document.getElementById("liveToast");
+toast.className = toast.className.replace("show", "");
+    }
+
+document.addEventListener('keydown', function (e){
+    if (e.key === 'Escape' || e.key === 'Esc') {
+        closeToast();
+    }
+})
