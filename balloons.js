8d16fa2c5c015c1a5dd30229801aa7b5a476755d
Owen Kitchener <okitchener@my.wctc.edu>
2026-02-02 08:37:04 -0600

    [2.16a] Attach date picker to text field

--- balloons.js
+++ balloons.js
@@ -1,3 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
   console.log("hello world!");
+  const elem = document.getElementById('dob');
+  const datepicker = new Datepicker(elem);
 });
