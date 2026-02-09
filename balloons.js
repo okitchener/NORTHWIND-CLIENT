738451cfd265dd9741b304044e435dc41a854b54
Owen Kitchener <okitchener@my.wctc.edu>
2026-02-09 08:19:51 -0600

    [3.09c] Toggle visibility for balloon image using checkbox

--- balloons.js
+++ balloons.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // event listener for check/uncheck
   document.getElementById('checkbox-card').addEventListener('change', function(e){
     if (e.target.classList.contains('form-check-input')) {
-      alert(e.target.id)
+            document.getElementById(e.target.id + 'Img').style.visibility = e.target.checked ? "visible" : "hidden";
     }
   });
 });
